df2143dff7ff78b9293b0ceb263036e6764bccb5
Lindsey Power <lindseynp123@gmail.com>
2025-03-04 16:01:25 -0500

    Update isfieldi.m

--- Utils/isfieldi.m
+++ Utils/isfieldi.m
@@ -1,3 +1,6 @@
+% Utility function written by Lindsey Power (March 2025) 
+% Required to run functions in bg_SWM.m
+
 function [val, cfgOut] = isfieldi(cfg, lst)
    for i=1:length(lst)
        field = lst{i};
